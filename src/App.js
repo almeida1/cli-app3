144e4a267889b7d4e7e82e7057dc1bc1e8042726
almeida1 <engsoftware1@gmail.com>
2025-03-24 14:23:18 -0300

    função - consulta cliente  Changes to be committed: 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/App.test.js 	new file:   src/componentes/consultar_cliente/ClienteConsulta.jsx 	new file:   src/componentes/consultar_cliente/ClienteConsultaStyles.css 	new file:   src/componentes/consultar_cliente/ClienteConsultaView.jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import ClienteCadastrarView from "./componentes/cadastrar_cliente/ClienteCadastrarView";
+import ClienteConsulta from "./componentes/consultar_cliente/ClienteConsulta";
 
-function App() {
+const App = () => {
+  const [exibirCadastro, setExibirCadastro] = useState(false);
+  const [exibirConsulta, setExibirConsulta] = useState(false);
+  const [clientes, setClientes] = useState([]);
+
+  const handleConsultar = () => {
+    setExibirConsulta(true);
+    setExibirCadastro(false); // Garante que o cadastro seja ocultado
+  };
+
+  const handleCadastrar = () => {
+    setExibirCadastro(true);
+    setExibirConsulta(false); // Garante que a consulta seja ocultada
+  };
+
+  const handleCancelarCadastro = () => {
+    setExibirCadastro(false);
+  };
+
+  const fetchClientes = async () => {
+    try {
+      const response = await fetch("http://localhost:8081/api/v1/clientes");
+      if (!response.ok) {
+        throw new Error("Falha ao buscar clientes");
+      }
+      const data = await response.json();
+      setClientes(data);
+    } catch (error) {
+      console.error("Erro ao buscar clientes:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (exibirConsulta) {
+      fetchClientes();
+    }
+  }, [exibirConsulta]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="title">Gerenciamento de Clientes</div>
+      <div className="button-container">
+        <button onClick={handleConsultar} className="button">
+          Consultar
+        </button>
+        <button onClick={handleCadastrar} className="button">
+          Cadastrar
+        </button>
+      </div>
+      {exibirCadastro && (
+        <ClienteCadastrarView onCancelar={handleCancelarCadastro} />
+      )}
+      {exibirConsulta && <ClienteConsulta clientes={clientes} />}
     </div>
   );
-}
+};
 
 export default App;
