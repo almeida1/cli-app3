4ac6681ea0579744a7621403ca53ba617f2c51b3
almeida1 <engsoftware1@gmail.com>
2025-04-12 14:04:40 -0300

    funcoes fatura

--- src/componentes/fatura_cadastrar/FaturaCadastrarView.jsx
+++ src/componentes/fatura_cadastrar/FaturaCadastrarView.jsx
@@ -1,10 +1,83 @@
-import React from "react";
+// Importando as dependências necessárias
+import React, { useState } from "react";
 import "./FaturaCadastrarStyles.css"; // Importando o CSS para estilização
 function FaturaCadastrarView() {
+  const [formData, setFormData] = useState({
+    numero: "",
+    CNPJdaContratada: "",
+    dataEmissao: "",
+    dataVencimento: "",
+    servicoContratado: "",
+    valor: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Data Submitted: ", formData);
+  };
+
   return (
     <div>
       <div className="title-cadastrar-view">Cadastrar Fatura</div>
-      {/* componente fatura */}
+      <form onSubmit={handleSubmit} className="fatura-form">
+        <div>
+          <label htmlFor="numero">Número:</label>
+          <input
+            type="text"
+            id="numero"
+            name="numero"
+            value={formData.numero}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="CNPJdaContratada">CNPJ:</label>
+          <input
+            type="text"
+            id="CNPJdaContratada"
+            name="CNPJdaContratada"
+            value={formData.CNPJdaContratada}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="dataVencimento">Data de Vencimento:</label>
+          <input
+            type="date"
+            id="dataVencimento"
+            name="dataVencimento"
+            value={formData.dataVencimento}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="servicoContratado">Serviço Contratado:</label>
+          <input
+            type="text"
+            id="servicoContratado"
+            name="servicoContratado"
+            value={formData.servicoContratado}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="valor">Valor:</label>
+          <input
+            type="number"
+            id="valor"
+            name="valor"
+            value={formData.valor}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Cadastrar</button>
+      </form>
     </div>
   );
 }
--- src/componentes/fatura_consultar/FaturaConsultarView.jsx
+++ src/componentes/fatura_consultar/FaturaConsultarView.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import "./FaturaConsultarStyles.css"; // Importando o CSS para estilização
-// Componente para consultar fatura
+import { useState } from "react";
+
 function FaturaConsultarView() {
   return (
     <div>
-      <div className="title-cadastrar-view">Consultar Fatura</div>
-      <form>
-        <div>
-          <label htmlFor="numeroFatura">Número da Fatura:</label>
-          <input type="text" id="numeroFatura" name="numeroFatura" />
-        </div>
-        <button type="submit">Consultar</button>
-      </form>
+      <h3> Consultar Fatura</h3>
     </div>
   );
 }
